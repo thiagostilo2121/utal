646941cbb2ecfcb52ebe3164002752c5fca38342
Thiago Stilo <robloxymasgame@gmail.com>
2025-03-04 13:48:32 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="utal",  # Nombre de tu librería
+    version="0.1.0",  # Versión de la librería
+    packages=find_packages(),  # Encuentra los paquetes de tu librería
+    install_requires=[  # Aquí especificas las dependencias
+        "selenium",  # Esto le dice a pip que instale Selenium automáticamente
+    ],
+    # Otras configuraciones opcionales
+    author="Tu Nombre",
+    author_email="stilothiagovalentin@gmail.com",
+    description="Unnoficial TikTok library for chatbot making. Use at your own risk.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiagostilo2121/utal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
