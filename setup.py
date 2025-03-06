f9481f5cfcc12da787d7e11d7262ad9d2fadc058
Thiago Developer <robloxymasgame@gmail.com>
2025-03-06 17:25:39 -0300

    0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="utal",  # Nombre de tu librería
-    version="0.2.1",  # Versión de la librería
+    version="0.2.2",  # Versión de la librería
     packages=find_packages(),  # Encuentra los paquetes de tu librería
     install_requires=[  # Aquí especificas las dependencias
         "selenium==4.29.0",  # Esto le dice a pip que instale Selenium automáticamente
